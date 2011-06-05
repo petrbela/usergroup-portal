80baf9bcd6d8e558b14b4ae2b2a3883995dd566f
Petr Bela <bela.petr@gmail.com>
2011-06-06 00:11:19 +0200

    Fixed problem with compiling annotated model objects

--- usergroup-portal/src/main/java/net/kindleit/gae/example/model/Message.java
+++ usergroup-portal/src/main/java/net/kindleit/gae/example/model/Message.java
@@ -2,10 +2,13 @@ package net.kindleit.gae.example.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Id;
+
 public class Message implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
     private Long id;
 
     private String text;
--- usergroup-portal/src/main/java/net/kindleit/gae/example/server/MessageRepository.java
+++ usergroup-portal/src/main/java/net/kindleit/gae/example/server/MessageRepository.java
@@ -1,61 +1,49 @@
 package net.kindleit.gae.example.server;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
-
-import javax.jdo.Extent;
-import javax.jdo.JDOHelper;
-import javax.jdo.PersistenceManager;
-import javax.jdo.PersistenceManagerFactory;
 
 import net.kindleit.gae.example.model.Message;
 import net.kindleit.gae.example.model.Messages;
 
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.Query;
+
 public class MessageRepository implements Messages {
 
-	PersistenceManagerFactory pmfInstance = JDOHelper.getPersistenceManagerFactory("transactions-optional");
+    public MessageRepository() {
+	ObjectifyService.register(Message.class);
+    }
 
-	/**
+    /**
      * @see net.kindleit.gae.example.model.Messages#getAll()
      */
-	public Collection<Message> getAll() {
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
-		try {
-			List<Message> messages = new ArrayList<Message>();
-		    Extent<Message> extent = pm.getExtent(Message.class, false);
-		    for (Message message : extent) {
-		        messages.add(message);
-		    }
-		    extent.closeAll();
-			
-		    return messages;
-		} finally {
-			pm.close();
-		}
+    public Collection<Message> getAll() {
+	Objectify ofy = ObjectifyService.begin();
+	List<Message> messages = new LinkedList<Message>();
+	Query<Message> q = ofy.query(Message.class);
+	for (Message m : q) {
+	    messages.add(m);
 	}
+	return messages;
+    }
 
-	/**
+    /**
      * @see net.kindleit.gae.example.model.Messages#create(net.kindleit.gae.example.model.Message)
      */
-	public void create(Message message) {
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
-		try {
-		    pm.makePersistent(message);
-		} finally {
-			pm.close();
-		}
-	}
+    public void create(final Message message) {
+	Objectify ofy = ObjectifyService.begin();
+	ofy.put(message);
+    }
 
-	/**
+    /**
      * @see net.kindleit.gae.example.model.Messages#deleteById(java.lang.Long)
      */
-	public void deleteById(Long id) {
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
-		try {
-			pm.deletePersistent(pm.getObjectById(Message.class, id));
-		} finally {
-			pm.close();
-		}
-	}
+    public void deleteById(final Long id) {
+	Objectify ofy = ObjectifyService.begin();
+	ofy.getDatastore().delete(new Key<Message>(Message.class, id).getRaw());
+    }
 }
