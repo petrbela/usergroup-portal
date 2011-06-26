12a8d1ffee9f94c2f6fee0a4e0acae48b63944c9
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2011-06-26 11:38:55 +0200

    init commit (for model package)

---
+++ usergroup-portal/src/main/java/ugportal/model/User.java
@@ -0,0 +1,18 @@
+/**
+ * User.java, Jun 26, 2011 11:38:22 AM 
+ */
+package ugportal.model;
+
+/**
+ * @author Ondrej Kvasnovsky
+ */
+public class User {
+
+    /**
+     * Creates new instance.
+     */
+    public User() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
