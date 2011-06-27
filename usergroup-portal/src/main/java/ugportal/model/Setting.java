31d7c37516bae77326181e65e2c822a819747e0a
Tomas Vantuch <tvantuch@gmail.com>
2011-06-27 20:31:06 +0200

    message

--- usergroup-portal/src/main/java/ugportal/model/Setting.java
+++ usergroup-portal/src/main/java/ugportal/model/Setting.java
@@ -11,7 +11,6 @@ public class Setting implements Serializable {
      */
     private static final long serialVersionUID = 1L;
     /**
-     * link to Twitter for downloading tweets
      */
     private Link linkTwitter;
     /**
